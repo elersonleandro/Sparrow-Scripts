86c251b79fa3075d602f694325aac06b0fe79e8b
DominiqueSPTech <dominique.dornan@sptech.school>
2024-10-21 17:01:49 -0300

    Captura de dados Python e Script Client (Linux, localhost)

---
+++ CapturaPython.py
@@ -0,0 +1,115 @@
+import psutil
+import time
+from mysql.connector import connect, Error
+
+config = {
+  'user': 'root',
+  'password': '#Gf23636497880',
+  'host': 'localhost',
+  'database': 'Sparrow'
+}
+
+                                                                                    
+def get_mac_address(interface_name='enX0'):
+    addrs = psutil.net_if_addrs()
+    if interface_name in addrs:
+        for addr in addrs[interface_name]:
+            if addr.family == psutil.AF_LINK:
+                return addr.address
+    return None
+
+mac_address = get_mac_address('enX0')  # ou 'wlan0' para Wi-Fi
+print(f"MAC Address: {mac_address}")
+
+
+
+
+try:
+        db = connect(**config)
+        if db.is_connected():
+            db_info = db.get_server_info()
+            print('Connected to MySQL server version -', db_info)
+                    
+            with db.cursor() as cursor:
+                query1 = (f"SELECT * FROM maquina WHERE endereco_mac = '{mac_address}'")
+                        
+                cursor.execute(query1)
+                    
+                
+                result = cursor.fetchone()
+                print(result)
+                    
+            cursor.close()
+            db.close()
+
+except Error as e:
+    print('Error to connect with MySQL -', e) 
+
+tamanho = len(result)
+
+
+if(tamanho > 0):
+    i = 0
+    a = 0
+    while i < 10: 
+        while a < 10:
+            cpu = psutil.cpu_percent(interval=None, percpu=False)        
+
+            ram = psutil.virtual_memory()[2]
+        
+            print(f"Uso da CPU: {cpu}%")
+            print(f"Uso da RAM: {ram}%")
+
+            try:
+                db = connect(**config)
+                if db.is_connected():
+                    db_info = db.get_server_info()
+                    print('Connected to MySQL server version -', db_info)
+                
+                with db.cursor() as cursor:
+                    query1 = ("INSERT INTO Sparrow.dado_capturado VALUES "
+                            f"(default, {cpu}, current_timestamp(), {result[0]}, 1)")
+                    query2 = ("INSERT INTO Sparrow.dado_capturado VALUES "
+                            f"(default, {ram}, current_timestamp(), {result[0]}, 2)")
+                    
+                    cursor.execute(query1)
+                    cursor.execute(query2)
+                    db.commit()
+                    print(cursor.rowcount, "registro inserido")
+                
+                cursor.close()
+                db.close()
+
+            except Error as e:
+                print('Error to connect with MySQL -', e) 
+
+            a += 1
+            i = 0
+            time.sleep(1)
+
+        disk = psutil.disk_usage("/")[3]
+
+        print('------------------------------------')
+        print(f"Espaço de disco disponível: {disk}%")
+
+        try:
+                db = connect(**config)
+                if db.is_connected():
+                    db_info = db.get_server_info()
+                    print('Connected to MySQL server version -', db_info)
+                
+                with db.cursor() as cursor:
+                    query1 = ("INSERT INTO Sparrow.dado_capturado VALUES "
+                            f"(default, {disk}, current_timestamp(), {result[0]}, 3)")
+                    
+                    cursor.execute(query1)
+                    db.commit()
+                    print(cursor.rowcount, "registro inserido")
+                
+                cursor.close()
+                db.close()
+
+        except Error as e:
+            print('Error to connect with MySQL -', e) 
+        i += 1
+        a = 0
---
+++ ScriptCliente.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+
+NC='\033[0m'
+
+echo -e "${GREEN}Atualizando o Linux${NC}"
+sudo apt update > /dev/null 2>&1 -y && sudo apt upgrade -y > /dev/null 2>&1
+
+echo -e "${GREEN}Fazendo instalação do Java${NC}"
+sudo apt install openjdk-17-jdk -y > /dev/null 2>&1
+
+echo -e "${GREEN}Fazendo pull do Kotlin e Python (GitHub)${NC}"
+git clone https://github.com/elersonleandro/Sparrow-Scripts.git > /dev/null 2>&1
+cd Sparrow-Scripts
+
+echo -e "${GREEN}Inicializando a captura pelo Kotlin${NC}"
+sudo java -jar Looca-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+echo -e "${GREEN}Fazendo instalação do Python${NC}"
+sudo apt install python3  -y > /dev/null 2>&1
+
+echo -e "${GREEN}Baixando e criando ambiente virtual venv${NC}"
+sudo apt install python3-venv > /dev/null 2>&1
+
+python3 -m venv venv > /dev/null 2>&1
+source venv/bin/activate
+
+echo -e "${GREEN}Baixando dependências${NC}"
+pip install psutil > /dev/null 2>&1
+pip install mysql-connector-python > /dev/null 2>&1
+
+echo -e "${GREEN}Inicializando a captura pelo Python${NC}"
+python3 CapturaPython.py
+
